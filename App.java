f5ab350eeee554253708609095950053bfdb5a0e
feralexei <fer_alexei196@hotmail.com>
2024-08-15 20:55:27 -0500

    Introduccion_POO

--- App.java
+++ App.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        //ObjetoPersona();
+        //UsandoConstructores();
+        //FiguraGeometrica();
         EjercicioComplementario();
     }
 
